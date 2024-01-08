ac51b083d6977c4381a09a1aa0cf46c725110444
franciscocesar1978 <franciscojosidsme@gmail.com>
2024-01-07 22:47:18 -0300

    atualização de euro icon

--- scripts.js
+++ scripts.js
@@ -69,7 +69,7 @@ function changeCurrency() {
 
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
-        currencyImage.src = "./assets/euro.png"
+        currencyImage.src = "./assets/Euro.png"
     }
 
 
